2ac5e97119429ceff0ae6589224433c0e8268838
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-06 12:41:45 +0100

    ruff check

--- kubernetes/routes/routes.py
+++ kubernetes/routes/routes.py
@@ -3,6 +3,8 @@ from services.get_worker_nodes import get_worker_nodes
 
 router = APIRouter()
 
+
 @router.get("/nodes")
 def nodes():
+    """Return all working nodes."""
     return get_worker_nodes()
--- kubernetes/services/get_worker_nodes.py
+++ kubernetes/services/get_worker_nodes.py
@@ -1,12 +1,14 @@
 from util.client_setup import get_api_client
 
+
 def get_worker_nodes():
+    """Return a JSON object with all the active working nodes."""
     api_client = get_api_client()
     nodes = api_client.list_node()
     worker_nodes = []
     for node in nodes.items:
         # Skip control plane
-        labels = str(node.metadata.labels) or "" #Empty if not exist instead of crash
+        labels = str(node.metadata.labels) or ""  # Empty if not exist instead of crash
         if "'node-role.kubernetes.io/control-plane': 'true'" in labels:
             continue
 
