834e058f92818eb9d572963c499072f259479269
MadsRantzau49 <madsrantzau2nd@gmail.com>
2026-03-11 10:57:14 +0100

    relative path

--- test/k3d/deploy.sh
+++ test/k3d/deploy.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-<<<<<<< HEAD
-kubectl apply -f /home/rantzau/uni/p6/kubernetes_application/src/cluster_api/manifest/
+kubectl apply -f src/cluster_api/manifest/
 
-=======
-kubectl apply -f manifests/
-
-kubectl wait --for=condition=available deployment --all --timeout=300s
->>>>>>> origin/init_test
